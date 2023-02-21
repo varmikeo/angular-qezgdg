275ab6ad32dd9daa673acfffbc216844bea8816a
varmikeo <126008076+varmikeo@users.noreply.github.com>
2023-02-21 22:11:35 +0300

    3 files modified, 3 added

--- src/app/products.ts
+++ src/app/products.ts
@@ -1,21 +1,23 @@
 export const products = [
   {
+    productId: 1,
     name: 'Phone XL',
     price: 799,
-    description: 'A large phone with one of the best screens'
+    description: 'A large phone with one of the best screens',
   },
   {
+    productId: 2,
     name: 'Phone Mini',
     price: 699,
-    description: 'A great phone with one of the best cameras'
+    description: 'A great phone with one of the best cameras',
   },
   {
+    productId: 3,
     name: 'Phone Standard',
     price: 299,
-    description: ''
-  }
+    description: '',
+  },
 ];
-
 
 /*
 Copyright Google LLC. All Rights Reserved.
