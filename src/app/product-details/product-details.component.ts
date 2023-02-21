d935be2c0b7ef0bdb5d997403c3700123821939d
varmikeo <126008076+varmikeo@users.noreply.github.com>
2023-02-21 22:24:43 +0300

    1 file modified, 1 added

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { products } from '../products';
+import { CartService } from '../cart.service';
 
 @Component({
   selector: 'app-product-details',
