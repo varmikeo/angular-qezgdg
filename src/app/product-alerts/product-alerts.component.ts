4fc12ac51ea62c06909df3d5b4d612eb0f8841ee
varmikeo <126008076+varmikeo@users.noreply.github.com>
2023-02-21 20:58:42 +0300

    1 file modified, 1 added

--- src/app/product-alerts/product-alerts.component.ts
+++ src/app/product-alerts/product-alerts.component.ts
@@ -7,9 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductAlertsComponent implements OnInit {
   @Input() product;
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
